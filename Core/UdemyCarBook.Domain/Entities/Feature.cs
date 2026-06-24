3af566955ce75a055f68dcb986f18612dcf42304
Furkan <sytrone@outlook.com.tr>
2026-06-25 02:13:48 +0300

    Persistence Katmanı eklendi. Migration işlemi yapıldı.

--- Core/UdemyCarBook.Domain/Entities/Feature.cs
+++ Core/UdemyCarBook.Domain/Entities/Feature.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace UdemyCarBook.Domain.Entities
 {
     public class Feature
     {
+        [Key]
         public int FutureId { get; set; }
         public string Name { get; set; }
 
---
+++ Infrastructure/UdemyCarBook.Persistence/Context/CarBookContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Persistence.Context
+{
+    public class CarBookContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-3S54QC0\\SQLEXPRESS; initial Catalog=UdemyCarBookDb; Integrated Security=true; TrustServerCertificate=True");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarDescription> CarDescriptions { get; set; }
+        public DbSet<CarFeature> CarFeatures { get; set; }
+        public DbSet<CarPricing> CarPricings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<FooterAddress> footerAddresses { get; set; }
+        public DbSet<Locations> Locations_ { get; set; }
+        public DbSet<Pricing> Pricings { get; set; }
+        public DbSet<Services> Services_ { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
