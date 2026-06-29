ad91f19aabc70869035e53301ad050d8a0b6a9b2
Furkan <sytrone@outlook.com.tr>
2026-06-29 13:37:26 +0300

    Banner entitysi Handler,Command,Queris ve result ksımına eklendi. BannerController oluşturuldu ve testleri yapıldı.

---
+++ Presentation/UdemyCarBook.WebApi/Controllers/BannerController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.AccessControl;
+using UdemyCarBook.Application.Features.CQRS.Commands.BannerCommands;
+using UdemyCarBook.Application.Features.CQRS.Handlers.BannerHandlers;
+using UdemyCarBook.Application.Features.CQRS.Queries.BannerQueries;
+
+namespace UdemyCarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannerController : ControllerBase
+    {
+        private readonly CreateBannerCommandHandlers _commandHandlers;
+        private readonly GetBannerByIdQueryHandler _queryHandler;
+        private readonly GetBannerQueryHandler _getBannerQueryHandler;
+        private readonly RemoveBannerCommandHandler _removeBannerCommandHandler;
+        private readonly UpdateBannerCommandHandler _updateBannerCommandHandler;
+
+        public BannerController(CreateBannerCommandHandlers commandHandlers, 
+            GetBannerByIdQueryHandler queryHandler, 
+            GetBannerQueryHandler getBannerQueryHandler, 
+            RemoveBannerCommandHandler removeBannerCommandHandler, 
+            UpdateBannerCommandHandler updateBannerCommandHandler)
+        {
+            _commandHandlers = commandHandlers;
+            _queryHandler = queryHandler;
+            _getBannerQueryHandler = getBannerQueryHandler;
+            _removeBannerCommandHandler = removeBannerCommandHandler;
+            _updateBannerCommandHandler = updateBannerCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BannerList()
+        {
+            var value = await _getBannerQueryHandler.Handle();
+            return Ok(value);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> BannerListById(int id)
+        {
+            var value = await _queryHandler.Handle(new GetBannerByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBannerCommandHandler(CreateBannerCommand command)
+        {
+            await _commandHandlers.Handle(command);
+            return Ok("Banner başarıyla oluşturuldu.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveBannerCommandHandler(int id)
+        {
+            await _removeBannerCommandHandler.Handle(new RemoveBannerCommands(id));
+            return Ok("Banner başarıyla silindi.");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateBannerCommandHandler(UpdateBannerCommand command)
+        {
+            await _updateBannerCommandHandler.Handle(command);
+            return Ok("Banner başarıyla güncellendi.");
+        }
+    }
+}
--- Presentation/UdemyCarBook.WebApi/Program.cs
+++ Presentation/UdemyCarBook.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.BannerHandlers;
 using UdemyCarBook.Application.Interfaces;
 using UdemyCarBook.Persistence.Context;
 using UdemyCarBook.Persistence.Repositories;
@@ -14,7 +15,16 @@ builder.Services.AddScoped<GetAboutByIdQueryHandlers>();
 builder.Services.AddScoped<GetAboutQueryHandlers>();
 builder.Services.AddScoped<RemoveAboutCommandHandler>();
 builder.Services.AddScoped<UpdateAboutCommandHandler>();
+
+
+builder.Services.AddScoped<CreateBannerCommandHandlers>();
+builder.Services.AddScoped<GetBannerQueryHandler>();
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
